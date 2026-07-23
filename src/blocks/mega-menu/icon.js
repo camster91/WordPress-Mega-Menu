6fd4d116ddf82d54d4d54675d437e44016a07181
Cameron Ashley <camster91@gmail.com>
2026-07-23 16:57:06 -0400

    Add icon.js

---
+++ src/blocks/mega-menu/icon.js
@@ -0,0 +1,10 @@
+/**
+ * SVG icon for the block placeholder and inserter.
+ */
+import { Path, SVG } from '@wordpress/components';
+
+export const megamenuIcon = (
+	<SVG width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+		<Path d="M5 5H3v2h2V5zm3 8h11v-2H8v2zm9-8H8v2h9V5zM7 11H5v2h2v-2zm0 8h2v-2H7v2zm3-2v2h11v-2H10z" />
+	</SVG>
+);
